41a7bb90695b9a46404a863f46053f67d0425681
Srinjay-hack <srinjaymondal@gmail.com>
2023-09-16 12:31:36 +0530

    fisrt

---
+++ src/components/pages/dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DashBoard = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'Right',
+                alignItems: 'Right',
+                height: '100vh'
+            }}
+        >
+            <h1>DashBoard</h1>
+        </div>
+    );
+};
+
+export default DashBoard;
